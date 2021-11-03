614f9ddbfc197e188a4b1c990813076e179ca135
assisnat <nas@ic.ufal.br>
2021-11-03 02:53:45 -0400

    Ultima alteraçao de cabeçalho

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "EU VOU SER A MAIS NOVA ESTAGIARIA DA WENI PORRAAAAAAA")
+        self.assertRegex(result.data.decode(), "Pipeline Devops - Nat")
 
 
 if __name__ == "__main__":
